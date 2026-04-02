fd85363ae28573a659aebed917d0c805a1d673d8
Mohammed Zahid Wadiwale <zahid@acturemedia.com>
2026-04-02 16:19:50 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,9 +5,9 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host:     process.env.DB_HOST     || "localhost",
   port:     parseInt(process.env.DB_PORT || "3306"),
-  user:     process.env.DB_USER     || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME     || "radhe_crm",
+  user:     process.env.DB_USER     || "U574372436_astha",
+  password: process.env.DB_PASSWORD || "Astha@123@Acture",
+  database: process.env.DB_NAME     || "U574372436_astha",
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0,
